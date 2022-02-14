4927aa9a312b731c18f8937da4293bed29f46f17
Kelelchi Nwandu <nwandukelechi@gmail.com>
2022-02-14 12:44:55 +0100

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const testData = [
 
 class App extends React.Component{
   state = {
-      profiles: testData,
+    profiles: testData,
   }
   render() {
     return (
--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component} from 'react';
+import axios from "axios";
 
 class Form extends Component{
-    userNameInput = React.createRef();
-    handleSubmit = (event) => {
+    state = { userName: '' };
+    handleSubmit = async (event) => {
         event.preventDefault();
-    }
+        const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`)
+        console.log(resp.data);
+    }; catch (error){
+        console.log(error)
+    };
+
     render(){
         return(
             <div>
-                <form action='' onSubmit={handleSubmit}>
-                    <input type="text" placeholder='Github Username' ref={this.userNameInput} required/>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" 
+                    placeholder='Github Username' 
+                    value={ this.state.userName } 
+                    onChange={ event => this.setState ({ userName: event.target.value }) }
+                    required/>
                     <button>Add card</button>
                 </form>
             </div>
